7aa0df2b46b2802a6fadc1029f64cec43fa061af
Dimitris Provatas <james.provatas@gmail.com>
2022-10-31 17:26:34 +0200

    new nvim configuration for macos

---
+++ MacOS/config/nvim/lua/sheepstress/plugins/lsp/mason.lua
@@ -0,0 +1,77 @@
+local mason_status, mason = pcall(require, "mason")
+if not mason_status then
+  return
+end
+
+local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
+if not mason_lspconfig_status then
+  return
+end
+
+local mason_null_ls_status, mason_null_ls = pcall(require, "mason-null-ls")
+if not mason_null_ls_status then
+  return
+end
+
+mason.setup()
+
+mason_lspconfig.setup({
+  ensure_installed = {
+    "awk_ls",
+    "bashls",
+    "clangd",
+    "csharp_ls",
+    "omnisharp_mono",
+    "omnisharp",
+    "cmake",
+    "cssls",
+    "cssmodules_ls",
+    "denols",
+    "dockerls",
+    "dotls",
+    "eslint",
+    "grammarly",
+    "graphql",
+    "html",
+    "jsonls",
+    "quick_lint_js",
+    "tsserver",
+    "ltex",
+    "sumneko_lua",
+    "marksman",
+    "intelephense",
+    "pylsp",
+    "rust_analyzer",
+    "sqls",
+    "taplo",
+    "tailwindcss",
+    "volar",
+    "wgsl_analyzer",
+    "lemminx",
+    "yamlls",
+  },
+})
+
+mason_null_ls.setup({
+  ensure_installed = {
+    "blade",
+    "clang_format",
+    "habolint",
+    "gitlint",
+    "prettier",
+    "ktlint",
+    "luacheck",
+    "vale",
+    "psalm",
+    "black",
+    "shellcheck",
+    "sql_formatter",
+    "taplo",
+    "vint",
+    "cfn_lint",
+    "codespell",
+    "cspell",
+    "editorconfig_checker",
+    "missspell",
+  },
+})
